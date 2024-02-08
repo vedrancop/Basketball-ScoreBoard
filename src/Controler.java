12c24ab36fc3fa55ec768f4c4e2f43f926a1e83f
vedrancop <146172096+vedrancop@users.noreply.github.com>
2024-02-08 16:22:20 +0100

    Adding Points

--- src/Controler.java
+++ src/Controler.java
@@ -1,2 +1,57 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class Controler {
+   
+    public Label HomeScore;
+    public Button H3;
+    public Button H2;
+    public Button H1;
+    public Button A3;
+    public Button A2;
+    public Button A1;
+    public Label AwayScore;
+
+    private int homeScore=0;
+    private int awayScore = 0;
+
+
+    public void H3p(ActionEvent actionEvent) {
+        for(int i=0; i<3 ; i++){
+            homeScore++;
+        }
+        HomeScore.setText(String.valueOf(homeScore));
+    }
+
+    public void H2p(ActionEvent actionEvent) {
+        for (int i = 0; i < 2; i++) {
+            homeScore++;
+        }
+        HomeScore.setText(String.valueOf(homeScore));
+    }
+
+    public void H1p(ActionEvent actionEvent) {
+        homeScore++;
+        HomeScore.setText(String.valueOf(homeScore));
+    }
+
+    public void A3p(ActionEvent actionEvent) {
+        for(int i=0; i<3 ; i++){
+            awayScore++;
+        }
+        AwayScore.setText(String.valueOf(awayScore));
+    }
+
+    public void A2p(ActionEvent actionEvent) {
+        for(int i=0; i<2 ; i++){
+            awayScore++;
+        }
+        AwayScore.setText(String.valueOf(awayScore));
+    }
+
+    public void A1p(ActionEvent actionEvent) {
+        awayScore++;
+        AwayScore.setText(String.valueOf((awayScore)));
+    }
 }
